a3b3a972556edbe820e9e3534481713a1377a474
mizech <michael.zech@hotmail.de>
2024-07-22 16:45:05 +0200

    Initial Commit

---
+++ WareHouseStock/WareHouseStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WareHouseStockApp.swift
+//  WareHouseStock
+//
+//  Created by Michael on 22.07.24.
+//
+
+import SwiftUI
+
+@main
+struct WareHouseStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
