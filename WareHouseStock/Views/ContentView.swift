579129ce4faf942acecd620ea31f75563af3f179
mizech <michael.zech@hotmail.de>
2024-07-27 12:32:30 +0200

    Added: AddVendor-Sheet

--- WareHouseStock/Views/ContentView.swift
+++ WareHouseStock/Views/ContentView.swift
@@ -2,8 +2,10 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) var context
-    @Query var vendors: [Vendor]
+    @Environment(\.modelContext) private var context
+    @Query private var vendors: [Vendor]
+    
+    @State private var isAddSheetShown = false
     
     var body: some View {
         NavigationStack {
@@ -35,19 +37,27 @@ struct ContentView: View {
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button(action: {
-                        let vendor = Vendor(name: "Vendor \(Date.timeIntervalSinceReferenceDate)", 
-                                            street: "Street 01", city: "City 01")
-                        let product1 = Product(name: "Product 01", vendor: vendor)
-                        let product2 = Product(name: "Product 02", vendor: vendor)
-                        let product3 = Product(name: "Product 03", vendor: vendor)
-                        context.insert(product1)
-                        context.insert(product2)
-                        context.insert(product3)
+                        isAddSheetShown.toggle()
                     }, label: {
                         Image(systemName: "plus")
                     })
                 }
             }
+            .sheet(isPresented: $isAddSheetShown, content: {
+                Button(action: {
+                    let vendor = Vendor(name: "Vendor \(Date.timeIntervalSinceReferenceDate)",
+                                        street: "Street 01", city: "City 01")
+                    let product1 = Product(name: "Product 01", vendor: vendor)
+                    let product2 = Product(name: "Product 02", vendor: vendor)
+                    let product3 = Product(name: "Product 03", vendor: vendor)
+                    context.insert(product1)
+                    context.insert(product2)
+                    context.insert(product3)
+                    isAddSheetShown.toggle()
+                }, label: {
+                    Text("Insert Vendor")
+                })
+            })
         }
     }
 }
