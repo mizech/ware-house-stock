9f8843646c0e0f17fbda4db9ddaf08a53fd0346d
mizech <michael.zech@hotmail.de>
2024-07-28 10:54:34 +0200

    Impl: Adding Products

---
+++ WareHouseStock/Views/ProductsListView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductsListView.swift
+//  WareHouseStock
+//
+//  Created by Michael on 28.07.24.
+//
+
+import SwiftUI
+
+struct ProductsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductsListView()
+}
